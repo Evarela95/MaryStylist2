b2fe5489c642f0c299f5907474f348709e68f422
esteb <esteb@DESKTOP-DQPOFDD>
2023-10-31 14:55:28 -0600

    Galeria(Falta carrusel) Citas(Identity)

---
+++ WebApplication1/Controllers/ImageController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ImageController : Controller
+    {
+        private string imagePath = "~/images/";
+
+        public ActionResult Index()
+        {
+            var imageDirectory = Server.MapPath(imagePath);
+            var imageNames = Directory.GetFiles(imageDirectory).Select(Path.GetFileName).ToList();
+            return View(imageNames);
+        }
+
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase file)
+        {
+            if (file != null && file.ContentLength > 0)
+            {
+                var fileName = Path.GetFileName(file.FileName);
+                var path = Path.Combine(Server.MapPath(imagePath), fileName);
+
+                file.SaveAs(path);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(string imageName)
+        {
+            var path = Path.Combine(Server.MapPath(imagePath), imageName);
+
+            if (System.IO.File.Exists(path))
+            {
+                System.IO.File.Delete(path);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult Galeria()
+        {
+            var imageDirectory = Server.MapPath("~/images");
+            var imageList = Directory.GetFiles(imageDirectory)
+                .Select(Path.GetFileName)
+                .ToList();
+
+            return View(imageList);
+        }
+
+
+      
+    }
+}
