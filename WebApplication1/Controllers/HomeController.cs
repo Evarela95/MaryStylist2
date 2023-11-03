beb6e96c1ba9143afd7be0fe24224c5f729c3c4c
Mateo Esquivel <mateo.esro1@gmail.com>
2023-11-02 22:29:59 -0600

    Olvido su contraseña, falta envio de correo para reestablecer la contraseña

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
@@ -20,11 +22,12 @@ namespace WebApplication1.Controllers
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult Contacto()
         {
             ViewBag.Message = "Your contact page.";
 
             return View();
         }
+
     }
 }
